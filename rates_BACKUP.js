b7064c2ceb23dbded1f629719bb4b5df5b7dec54
restechCLL <120662598+restechCLL@users.noreply.github.com>
2023-01-20 14:17:24 -0600

    Add files via upload

---
+++ rates_BACKUP.js
@@ -0,0 +1,94 @@
+//////////////////////////////////////////////
+// EDIT RATES AND LINKS BELOW //
+/////////////////////////////////////////////
+
+// APARTMENT 2 BEDROOMS
+const price_apt2Bed = 5260;
+const url_apt2Bed = "https://www.baylor.edu/cll/index.php?id=983475";
+
+// APARTMENT 3 BEDROOMS
+const price_apt3Bed = 4800;
+const url_apt3Bed = "https://www.baylor.edu/cll/index.php?id=983474";
+
+// APARTMENT 4 BEDROOMS
+const price_apt4Bed = 3920;
+const url_apt4Bed = "https://www.baylor.edu/cll/index.php?id=983473";
+
+// APARTMENT DOUBLE GOLD
+const price_aptDoubleGold = 4570;
+const url_aptDoubleGold = "https://www.baylor.edu/cll/index.php?id=983470";
+
+// APARTMENT SINGLE GOLD
+const price_aptSingleGold = 5820;
+const url_aptSingleGold = "https://www.baylor.edu/cll/index.php?id=983469";
+
+// DOUBLE ROOM
+const price_DoubleRoom = 3670;
+const url_DoubleRoom = "https://www.baylor.edu/cll/index.php?id=983461";
+
+// DOUBLE ROOM PLUS
+const price_DoubleRoomPlus = 3753;
+const url_DoubleRoomPlus = "https://www.baylor.edu/cll/index.php?id=983471";
+
+// DOUBLE ROOM PRIVATE BATH
+const price_DoubleRoomPrivateBath = 4440;
+const url_DoubleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983463";
+
+// DOUBLE ROOM SHARED BATH
+const price_DoubleRoomSharedBath = 4200;
+const url_DoubleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=983459";
+
+// DOUBLE ROOM SHARED BATH PLUS
+const price_DoubleRoomSharedBathPlus = 4340;
+const url_DoubleRoomSharedBathPlus = "https://www.baylor.edu/cll/index.php?id=983472";
+
+// DOUBLE SUITE SHARED BATH
+const price_DoubleSuiteSharedBath = 4340;
+const url_DoubleSuiteSharedBath = "https://www.baylor.edu/cll/index.php?id=983477";
+
+// ONE BEDROOM SUITE
+const price_OneBedroomSuite = 5240;
+const url_OneBedroomSuite = "";
+
+// SINGLE ROOM
+const price_SingleRoom = 4870;
+const url_SingleRoom = "https://www.baylor.edu/cll/index.php?id=983468";
+
+// SINGLE ROOM PRIVATE BATH
+const price_SingleRoomPrivateBath = 5660;
+const url_SingleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983460";
+
+// SINGLE SUITE SHARED BATH
+const price_SingleSuiteSharedBath = 4950;
+const url_SingleSuiteSharedBath = "https://www.baylor.edu/cll/index.php?id=983476";
+
+// SUPER SINGLE WITH BATH
+const price_SuperSingleWithBath = 4700;
+const url_SuperSingleWithBath = "";
+
+// TRIPLE ROOM
+const price_TripleRoom = 3520;
+const url_TripleRoom = "https://www.baylor.edu/cll/index.php?id=983464";
+
+// TRIPLE EFFICIENCY
+const price_TripleEfficiency = 3475;
+const url_TripleEfficiency = "https://www.baylor.edu/cll/index.php?id=983480";
+
+// TRIPLE ROOM SHARED BATH
+const price_TripleRoomSharedBath = 3670;
+const url_TripleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=983467";
+
+
+
+
+//////////////////////////////////
+// CURRENCY - DO NOT EDIT //
+//////////////////////////////////
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 0,
+});
+
+//////////////////////////////////////////////
+/////////////////////////////////////////////
