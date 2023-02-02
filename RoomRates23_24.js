3c5a5c8e8edf839dd8bed57d00a00db6a1fd5bdd
restechCLL <120662598+restechCLL@users.noreply.github.com>
2023-02-02 14:26:52 -0600

    Update RoomRates23_24.js

--- RoomRates23_24.js
+++ RoomRates23_24.js
@@ -7,83 +7,83 @@ const price_apt2Bed = "Coming Soon";
 const url_apt2Bed = "https://www.baylor.edu/cll/index.php?id=983475";
 
 // APARTMENT 3 BEDROOMS
-const price_apt3Bed = 4800;
+const price_apt3Bed = "Coming Soon";
 const url_apt3Bed = "https://www.baylor.edu/cll/index.php?id=983474";
 
 // APARTMENT 4 BEDROOMS
-const price_apt4Bed = 3920;
+const price_apt4Bed = "Coming Soon";
 const url_apt4Bed = "https://www.baylor.edu/cll/index.php?id=983473";
 
 // APARTMENT DOUBLE GOLD
-const price_aptDoubleGold = 4570;
+const price_aptDoubleGold = "Coming Soon";
 const url_aptDoubleGold = "https://www.baylor.edu/cll/index.php?id=983470";
 
 // APARTMENT SINGLE GOLD
-const price_aptSingleGold = 5820;
+const price_aptSingleGold = "Coming Soon";
 const url_aptSingleGold = "https://www.baylor.edu/cll/index.php?id=983469";
 
 // DOUBLE ROOM
-const price_DoubleRoom = 3670;
+const price_DoubleRoom = "Coming Soon";
 const url_DoubleRoom = "https://www.baylor.edu/cll/index.php?id=983461";
 
 // DOUBLE ROOM PLUS
-const price_DoubleRoomPlus = 3753;
+const price_DoubleRoomPlus = "Coming Soon";
 const url_DoubleRoomPlus = "https://www.baylor.edu/cll/index.php?id=983471";
 
 // DOUBLE ROOM PRIVATE BATH
-const price_DoubleRoomPrivateBath = 4440;
+const price_DoubleRoomPrivateBath = "Coming Soon";
 const url_DoubleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983463";
 
 // DOUBLE ROOM SHARED BATH
-const price_DoubleRoomSharedBath = 4200;
+const price_DoubleRoomSharedBath = "Coming Soon";
 const url_DoubleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=983459";
 
 // DOUBLE ROOM SHARED BATH PLUS
-const price_DoubleRoomSharedBathPlus = 4340;
+const price_DoubleRoomSharedBathPlus = "Coming Soon";
 const url_DoubleRoomSharedBathPlus = "https://www.baylor.edu/cll/index.php?id=983472";
 
 // DOUBLE SUITE SHARED BATH
-const price_DoubleSuiteSharedBath = 4340;
+const price_DoubleSuiteSharedBath = "Coming Soon";
 const url_DoubleSuiteSharedBath = "https://www.baylor.edu/cll/index.php?id=983477";
 
 // ONE BEDROOM SUITE
-const price_OneBedroomSuite = 5240;
+const price_OneBedroomSuite = "Coming Soon";
 const url_OneBedroomSuite = "";
 
 // SINGLE ROOM
-const price_SingleRoom = 4870;
+const price_SingleRoom = "Coming Soon";
 const url_SingleRoom = "https://www.baylor.edu/cll/index.php?id=983468";
 
 // SINGLE ROOM PRIVATE BATH
-const price_SingleRoomPrivateBath = 5660;
+const price_SingleRoomPrivateBath = "Coming Soon";
 const url_SingleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983460";
 
 // SINGLE ROOM SHARED BATH
-const price_SingleRoomSharedeBath = 5080;
+const price_SingleRoomSharedeBath = "Coming Soon";
 const url_SingleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=987331";
 
 // SINGLE SUITE SHARED BATH
-const price_SingleSuiteSharedBath = 4950;
+const price_SingleSuiteSharedBath = "Coming Soon";
 const url_SingleSuiteSharedBath = "https://www.baylor.edu/cll/index.php?id=983476";
 
 // SUPER SINGLE WITH BATH
-const price_SuperSingleWithBath = 4700;
+const price_SuperSingleWithBath = "Coming Soon";
 const url_SuperSingleWithBath = "";
 
 // TRIPLE ROOM
-const price_TripleRoom = 3520;
+const price_TripleRoom = "Coming Soon";
 const url_TripleRoom = "https://www.baylor.edu/cll/index.php?id=983464";
 
 // TRIPLE EFFICIENCY
-const price_TripleEfficiency = 3475;
+const price_TripleEfficiency = "Coming Soon";
 const url_TripleEfficiency = "https://www.baylor.edu/cll/index.php?id=983480";
 
 // TRIPLE ROOM SHARED BATH
-const price_TripleRoomSharedBath = 3670;
+const price_TripleRoomSharedBath = "Coming Soon";
 const url_TripleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=983467";
 
 // TRIPLE ROOM PRIVATE BATH
-const price_TripleRoomPrivateBath = 3850;
+const price_TripleRoomPrivateBath = "Coming Soon";
 const url_TripleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983467";
 
 
