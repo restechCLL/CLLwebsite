e4aa250c5b214034aa4fab930007eec58887ee3a
ian-philbrick <123660395+ian-philbrick@users.noreply.github.com>
2023-02-28 13:39:47 -0600

    Update RoomRates23_24.js

--- RoomRates23_24.js
+++ RoomRates23_24.js
@@ -3,51 +3,51 @@
 /////////////////////////////////////////////
 
 // APARTMENT 2 BEDROOMS
-const price_apt2Bed = "$5,520";
+const price_apt2Bed = "$5,520 per semester";
 const url_apt2Bed = "https://www.baylor.edu/cll/index.php?id=983475";
 
 // APARTMENT 3 BEDROOMS
-const price_apt3Bed = "$5,040";
+const price_apt3Bed = "$5,040 per semester";
 const url_apt3Bed = "https://www.baylor.edu/cll/index.php?id=983474";
 
 // APARTMENT 4 BEDROOMS
-const price_apt4Bed = "$4,120";
+const price_apt4Bed = "$4,120 per semester";
 const url_apt4Bed = "https://www.baylor.edu/cll/index.php?id=983473";
 
 // APARTMENT DOUBLE GOLD
-const price_aptDoubleGold = "$4,800";
+const price_aptDoubleGold = "$4,800 per semester";
 const url_aptDoubleGold = "https://www.baylor.edu/cll/index.php?id=983470";
 
 // APARTMENT SINGLE GOLD
-const price_aptSingleGold = "$6,110";
+const price_aptSingleGold = "$6,110 per semester";
 const url_aptSingleGold = "https://www.baylor.edu/cll/index.php?id=983469";
 
 // DOUBLE ROOM
-const price_DoubleRoom = "$3,850";
+const price_DoubleRoom = "$3,850 per semester";
 const url_DoubleRoom = "https://www.baylor.edu/cll/index.php?id=983461";
 
 // DOUBLE ROOM PLUS
-const price_DoubleRoomPlus = "$3,940";
+const price_DoubleRoomPlus = "$3,940 per semester";
 const url_DoubleRoomPlus = "https://www.baylor.edu/cll/index.php?id=983471";
 
 // DOUBLE ROOM PRIVATE BATH
-const price_DoubleRoomPrivateBath = "$4,660";
+const price_DoubleRoomPrivateBath = "$4,660 per semester";
 const url_DoubleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983463";
 
 // DOUBLE ROOM SHARED BATH
-const price_DoubleRoomSharedBath = "$4,410";
+const price_DoubleRoomSharedBath = "$4,410 per semester";
 const url_DoubleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=983459";
 
 // DOUBLE ROOM SHARED BATH PLUS
-const price_DoubleRoomSharedBathPlus = "$4,560";
+const price_DoubleRoomSharedBathPlus = "$4,560 per semester";
 const url_DoubleRoomSharedBathPlus = "https://www.baylor.edu/cll/index.php?id=983472";
 
 // DOUBLE SUITE SHARED BATH
-const price_DoubleSuiteSharedBath = "$4,560";
+const price_DoubleSuiteSharedBath = "$4,560 per semester";
 const url_DoubleSuiteSharedBath = "https://www.baylor.edu/cll/index.php?id=983477";
 
 // DOUBLE ROOM POD BATH
-const price_DoubleRoomPodBath = "$4,100";
+const price_DoubleRoomPodBath = "$4,100 per semester";
 const url_DoubleRoomPodBath = "https://www.baylor.edu/cll/index.php?id=987364";
 
 // ONE BEDROOM SUITE
@@ -55,19 +55,19 @@ const price_OneBedroomSuite = "Coming Soon";
 const url_OneBedroomSuite = "";
 
 // SINGLE ROOM
-const price_SingleRoom = "$5,110";
+const price_SingleRoom = "$5,110 per semester";
 const url_SingleRoom = "https://www.baylor.edu/cll/index.php?id=983468";
 
 // SINGLE ROOM PRIVATE BATH
-const price_SingleRoomPrivateBath = "$5,940";
+const price_SingleRoomPrivateBath = "$5,940 per semester";
 const url_SingleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983460";
 
 // SINGLE ROOM SHARED BATH
-const price_SingleRoomSharedeBath = "$5,080";
+const price_SingleRoomSharedeBath = "$5,080 per semester";
 const url_SingleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=987331";
 
 // SINGLE SUITE SHARED BATH
-const price_SingleSuiteSharedBath = "$5,200";
+const price_SingleSuiteSharedBath = "$5,200 per semester";
 const url_SingleSuiteSharedBath = "https://www.baylor.edu/cll/index.php?id=983476";
 
 // SUPER SINGLE WITH BATH
@@ -75,7 +75,7 @@ const price_SuperSingleWithBath = "Coming Soon";
 const url_SuperSingleWithBath = "";
 
 // TRIPLE ROOM
-const price_TripleRoom = "$3,695";
+const price_TripleRoom = "$3,695 per semester";
 const url_TripleRoom = "https://www.baylor.edu/cll/index.php?id=983464";
 
 // TRIPLE EFFICIENCY
@@ -87,7 +87,7 @@ const price_TripleRoomSharedBath = "Coming Soon";
 const url_TripleRoomSharedBath = "https://www.baylor.edu/cll/index.php?id=983467";
 
 // TRIPLE ROOM PRIVATE BATH
-const price_TripleRoomPrivateBath = "$3,850";
+const price_TripleRoomPrivateBath = "$3,850 per semester";
 const url_TripleRoomPrivateBath = "https://www.baylor.edu/cll/index.php?id=983467";
 
 
